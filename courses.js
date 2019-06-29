4e6d9632df86c30ebd62aa8bba40b8e231c34fd5
Pablo Quiceno <jpquiceno@kometsales.com>
2019-06-29 13:09:08 -0500

    Ejemplo con Apollo Server

---
+++ courses.js
@@ -0,0 +1,7 @@
+module.exports = [
+  {id: "1", title: "Curso de Graphql", views: 1000},
+  {id: "2", title: "Curso de Javascript", views: 4000},
+  {id: "3", title: "Curso de Node Js", views: 1213},
+  {id: "4", title: "Curso de Go", views: 3242},
+  {id: "5", title: "Curso de Ruby", views: 2424}
+]
